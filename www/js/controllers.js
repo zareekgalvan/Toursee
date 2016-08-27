1c7679d7f3c985a8864926248b09af4f39b9be96
omarcarreon <oomarcarreon@gmail.com>
2016-08-27 16:25:27 -0500

    bootstrap, cordova location

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,64 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ['ionic','ngCordova'])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($scope, $http, $cordovaGeolocation) {
+
+  var globalLat = null;
+  var globalLong = null;
+  
+  function getPlaces(){
+    if (globalLong != null && globalLat != null){
+      $http({
+          method: 'GET',
+          url: "https://api.foursquare.com/v2/venues/search?ll="+globalLat+","+globalLong+"&client_id=ZH3UCNE24YVXYSAPJN2KIMAMHERNULWIE0ZP1MDKIZYZFKMO&client_secret=4VTHBQDRNBXNQ4KIAUCMW50UMCFLPYT13V5C3OIWESBY4FMF&v=20131017"
+        })
+        .success(function(data, status, headers, config) {
+          console.log(data);
+        })
+        .error(function(data, status, headers, config) {
+          alert("Error");
+        })
+    }
+    
+  }
+
+  function getUserLocation(){
+    var posOptions = {timeout: 10000, enableHighAccuracy: false};
+    $cordovaGeolocation
+    .getCurrentPosition(posOptions)
+
+    .then(function (position) {
+      globalLat  = position.coords.latitude
+      globalLong = position.coords.longitude
+      getPlaces();
+
+    }, function(err) {
+      console.log(err)
+    });
+
+    var watchOptions = {timeout : 3000, enableHighAccuracy: false};
+    var watch = $cordovaGeolocation.watchPosition(watchOptions);
+
+    watch.then(
+      null,
+
+      function(err) {
+         console.log(err)
+      },
+
+      function(position) {
+         globalLat = position.coords.latitude
+         globalLong = position.coords.longitude
+      }
+    );
+
+    watch.clearWatch();
+  }
+
+    
+
+  getUserLocation();
+
+})
 
 .controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
